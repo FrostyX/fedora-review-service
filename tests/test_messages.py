2b4eb596d1cd7042c0b0f839b676d44f18efcbf6
Jakub Kadlcik <frostyx@email.cz>
2022-11-27 22:33:18 +0100

    Add a test file

---
+++ tests/test_messages.py
@@ -0,0 +1,77 @@
+import os
+import json
+import unittest
+from fedora_messaging.api import Message
+from fedora_review_service.helpers import find_srpm_url, review_package_name
+from fedora_review_service.copr import Copr
+
+
+class MessageTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.here = os.path.dirname(os.path.abspath(__file__))
+        self.data = os.path.join(self.here, "data")
+
+    def get_message(self, name):
+        path = os.path.join(self.data, name)
+        data = json.load(open(path, "r"))
+        del data["id"]
+        del data["queue"]
+        return Message(**data)
+
+
+class TestMessages(MessageTestCase):
+
+    def test_review_package_name(self):
+        summary = "Review Request: libgbinder - C interfaces for Android binder"
+        assert review_package_name(summary) == "libgbinder"
+
+    def test_find_srpm_url(self):
+        message = self.get_message("bugzilla-contributor-srpm-update.json")
+        packagename = review_package_name(message.body["bug"]["summary"])
+        srpm_url = find_srpm_url(packagename, message.body["comment"]["body"])
+        assert srpm_url == (
+            "https://download.copr.fedorainfracloud.org"
+            "/results/aleasto/waydroid/fedora-rawhide-x86_64/05068987-libgbinder"
+            "/libgbinder-1.1.29-1.fc38.src.rpm"
+        )
+
+
+class TestCopr(MessageTestCase):
+
+    def test_render_bugzilla_comment(self):
+        message = self.get_message("copr-review-build-end.json")
+        expected = (
+            "Copr build:\n"
+            "https://copr.fedorainfracloud.org/coprs/build/5069760\n"
+            "(succeeded)\n"
+            "\n"
+            "Review template:\n"
+            "https://download.copr.fedorainfracloud.org/results/frostyx/"
+            "fedora-review-2120131-libgbinder/fedora-rawhide-x86_64/"
+            "05069760-libgbinder/fedora-review/review.txt\n"
+        )
+
+        # FIXME I don't want this \n there, I should fix the code
+        expected += "\n"
+
+        copr = Copr(message)
+        assert copr.render_bugzilla_comment() == expected
+
+    def test_ignore(self):
+        name = "copr-review-build-end.json"
+
+        message = self.get_message(name)
+        assert Copr(message).ignore is False
+
+        message = self.get_message(name)
+        message.body["chroot"] = "fedora-37-x86_64"
+        assert Copr(message).ignore is True
+
+        message = self.get_message(name)
+        message.body["owner"] = "jdoe"
+        assert Copr(message).ignore is True
+
+        message = self.get_message(name)
+        message.body["copr"] = "coprname-not-related-to-review"
+        assert Copr(message).ignore is True
