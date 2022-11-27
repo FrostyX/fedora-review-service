08ae3398481c921bc282bfd7d8b6aa93df99cac3
Jakub Kadlcik <frostyx@email.cz>
2022-11-27 22:32:20 +0100

    Create a class for parsing Copr messages

--- fedora_review_service/consumer.py
+++ fedora_review_service/consumer.py
@@ -3,7 +3,12 @@
 import jinja2
 from fedora_messaging.api import consume
 from fedora_messaging.config import conf
-from fedora_review_service.helpers import find_srpm_url, submit_to_copr
+from fedora_review_service.helpers import (
+    find_srpm_url,
+    submit_to_copr,
+    review_package_name,
+)
+from fedora_review_service.copr import Copr
 
 
 conf.setup_logging()
@@ -25,43 +30,10 @@ def dispatch(message):
 
 
 def handle_copr_message(message):
-    chroot = message.body["chroot"]
-    if chroot != "fedora-rawhide-x86_64":
+    copr = Copr(message)
+    if copr.ignore:
         return
-
-    status = message.body["status"]
-    if status not in [0, 1]:
-        # The build is still running, we want only the last chroot
-        return
-
-    ownername = message.body["owner"]
-    if ownername != "frostyx":
-        return
-
-    projectname = message.body["copr"]
-    rhbz = rhbz_number(projectname)
-    if not rhbz:
-        return
-
-    build_id = message.body["build"]
-    status_text = "failed" if status == 0 else "succeeded"
-    packagename = message.body["pkg"]
-
-    build_url = get_build_url(build_id)
-    review_template_url = get_review_template_url(
-        build_id, ownername, projectname, packagename, chroot)
-
-    # Parse issues and [!] checkboxes from review.json once the JSON
-    # support for fedora-review is merged and released
-    # https://pagure.io/FedoraReview/pull-request/463
-    issues = ["First issue", "Second issue"]
-
-    comment = render_bugzilla_comment(
-        build_url,
-        status_text,
-        review_template_url,
-        issues,
-    )
+    comment = copr.render_bugzilla_comment()
     submit_bugzilla_comment(comment)
 
 
@@ -86,54 +58,14 @@ def handle_bugzilla_message(message):
     # TODO If not already fedora-review+
     bug["flags"]
 
-    srpm_url = find_srpm_url(comment["body"])
+    packagename = review_package_name(bug["summary"])
+    srpm_url = find_srpm_url(packagename, comment["body"])
     if not srpm_url:
         return
 
     # Until we farm all the test files we need
     save_message(message)
-    submit_to_copr(bug["id"], bug["summary"], srpm_url)
-
-
-def get_build_url(build_id):
-    url = "https://copr.fedorainfracloud.org/coprs/build/{0}"
-    return url.format(build_id)
-
-
-def get_review_template_url(build_id, owner, project, package, chroot):
-    base = "https://download.copr.fedorainfracloud.org/results"
-    fullname = f"{owner}/{project}"
-    destdir = f"{build_id:08d}-{package}"
-    return f"{base}/{fullname}/{chroot}/{destdir}/fedora-review/review.txt"
-
-
-def rhbz_number(projectname):
-    """
-    We should instead have a database mapping BUILD_ID to RHBZ_ID
-    but that's too much work for a prototype
-    """
-    split = projectname.split("-", 3)
-    if len(split) < 4:
-        return None
-    if split[0] != "fedora" and split[1] != "review":
-        return None
-    if not split[2].isnumeric():
-        return None
-    return split[2]
-
-
-def render_bugzilla_comment(build_url, build_status,
-                            review_template_url, issues):
-    loader = jinja2.FileSystemLoader(".")
-    env = jinja2.Environment(loader=loader)
-    template = env.get_template("bugzilla-comment.j2")
-    values = {
-        "build_url": build_url,
-        "build_status": build_status,
-        "review_template_url": review_template_url,
-        "issues": issues,
-    }
-    return template.render(**values)
+    submit_to_copr(bug["id"], packagename, srpm_url)
 
 
 def submit_bugzilla_comment(text):
---
+++ fedora_review_service/copr.py
@@ -0,0 +1,86 @@
+import jinja2
+
+
+class Copr:
+
+    def __init__(self, message):
+        self.message = message
+        self.build_id = message.body["build"]
+        self.chroot = message.body["chroot"]
+        self.status = message.body["status"]
+        self.ownername = message.body["owner"]
+        self.projectname = message.body["copr"]
+        self.packagename = message.body["pkg"]
+
+    @property
+    def fullname(self):
+        return "{0}/{1}".format(self.ownername, self.projectname)
+
+    @property
+    def build_url(self):
+        url = "https://copr.fedorainfracloud.org/coprs/build/{0}"
+        return url.format(self.build_id)
+
+    @property
+    def review_template_url(self):
+        return "{0}/{1}".format(self.review_dir_url, "review.txt")
+
+    @property
+    def review_dir_url(self):
+        base = "https://download.copr.fedorainfracloud.org/results"
+        destdir = f"{self.build_id:08d}-{self.packagename}"
+        return f"{base}/{self.fullname}/{self.chroot}/{destdir}/fedora-review"
+
+    @property
+    def status_text(self):
+        return "failed" if self.status == 0 else "succeeded"
+
+    @property
+    def issues(self):
+        # Parse issues and [!] checkboxes from review.json once the JSON
+        # support for fedora-review is merged and released
+        # https://pagure.io/FedoraReview/pull-request/463
+        return []
+
+    @property
+    def rhbz_number(self):
+        """
+        We should instead have a database mapping BUILD_ID to RHBZ_ID
+        but that's too much work for a prototype
+        """
+        split = self.projectname.split("-", 3)
+        if len(split) < 4:
+            return None
+        if split[0] != "fedora" and split[1] != "review":
+            return None
+        if not split[2].isnumeric():
+            return None
+        return split[2]
+
+    def render_bugzilla_comment(self):
+        loader = jinja2.FileSystemLoader(".")
+        env = jinja2.Environment(loader=loader)
+        template = env.get_template("bugzilla-comment.j2")
+        values = {
+            "build_url": self.build_url,
+            "build_status": self.status_text,
+            "review_template_url": self.review_template_url,
+            "issues": self.issues,
+        }
+        return template.render(**values)
+
+    @property
+    def ignore(self):
+        if self.chroot != "fedora-rawhide-x86_64":
+            return True
+
+        # The build is still running, we want only the last chroot
+        if self.status not in [0, 1]:
+            return True
+
+        if self.ownername != "frostyx":
+            return True
+
+        if not self.rhbz_number:
+            return True
+        return False
